bbc2cae509b72a12ddc4967320a4c4e382217dff
HarrierPanels <adsnse@gmail.com>
2023-11-29 09:30:36 +0000

    Initial commit

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo  ===========
+if [ "$(id -u)" -ne 0 ]; then
+  CMD="sudo"
+else
+  CMD=""
+fi
+
+BASE_URL="https://raw.githubusercontent.com/HarrierPanels/ks8/main"
+SCRIPTS=("install-docker.sh" "install-nginx.sh" "install-playpit.sh")
+
+for SCRIPT in "${SCRIPTS[@]}"; do
+  curl -s "${BASE_URL}/${SCRIPT}" | ${CMD} bash
+done
